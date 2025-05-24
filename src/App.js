afc965ca07269b4d7e8c8fb331349430a5c59225
Yanqin Qualmann <yq.qualmann@gmail.com>
2025-05-24 11:52:33 +0200

    add lock

--- src/App.js
+++ src/App.js
@@ -1,111 +1,98 @@
-
-import './App.css';
-import React from "react"
-import Sidebar from "./components/Sidebar"
-import Editor from "./components/Editor"
+import "./App.css";
+import React from "react";
+import Sidebar from "./components/Sidebar";
+import Editor from "./components/Editor";
 //import {data} from "./data"
-import Split from "react-split"
-import {nanoid} from "nanoid"
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { icon } from '@fortawesome/fontawesome-svg-core/import.macro'
-
-
-
-
-
-
+import Split from "react-split";
+import { nanoid } from "nanoid";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { icon } from "@fortawesome/fontawesome-svg-core/import.macro";
 
 export default function App() {
-    //When the app first loads, initialize the notes state with the notes saved in localStorage
-    //when the array is empty, returns[] instead undefined as an error.
-    //'() =>'is a lazy State initialization function that ensures react to reach to localstorage only at the firsttime when app loads
-  const [notes, setNotes] = React.useState(() => JSON.parse(localStorage.getItem("notes")) || []) 
+  //When the app first loads, initialize the notes state with the notes saved in localStorage
+  //when the array is empty, returns[] instead undefined as an error.
+  //'() =>'is a lazy State initialization function that ensures react to reach to localstorage only at the firsttime when app loads
+  const [notes, setNotes] = React.useState(
+    () => JSON.parse(localStorage.getItem("notes")) || []
+  );
   const [currentNoteId, setCurrentNoteId] = React.useState(
-      (notes[0] && notes[0].id) || ""
-  )
- 
+    (notes[0] && notes[0].id) || ""
+  );
+
   // Every time the `notes` array changes, save it in localStorage.
   React.useEffect(() => {
-    localStorage.setItem("notes", JSON.stringify(notes))
-}, [notes])
+    localStorage.setItem("notes", JSON.stringify(notes));
+  }, [notes]);
 
   function createNewNote() {
-      const newNote = {
-          id: nanoid(),
-          body: "# Type your markdown note's title here"
-      }
-      setNotes(prevNotes => [newNote, ...prevNotes])
-      setCurrentNoteId(newNote.id)   //update state 'currentNoteId' to 'newNote.id' 
+    const newNote = {
+      id: nanoid(),
+      body: "# New note",
+    };
+    setNotes((prevNotes) => [newNote, ...prevNotes]);
+    setCurrentNoteId(newNote.id); //update state 'currentNoteId' to 'newNote.id'
   }
-  
+
   function updateNote(text) {
-      // Put the most recently-modified note at the top
-      setNotes(oldNotes => {
-        const newArray = []
-        for(let i = 0; i < oldNotes.length; i++) {
-            const oldNote = oldNotes[i]
-            if(oldNote.id === currentNoteId) {
-                newArray.unshift({ ...oldNote, body: text }) //update the body property, but keep all the rest properties.
-            } else {
-                newArray.push(oldNote)
-            }
+    // Put the most recently-modified note at the top
+    setNotes((oldNotes) => {
+      const newArray = [];
+      for (let i = 0; i < oldNotes.length; i++) {
+        const oldNote = oldNotes[i];
+        if (oldNote.id === currentNoteId) {
+          newArray.unshift({ ...oldNote, body: text }); //update the body property, but keep all the rest properties.
+        } else {
+          newArray.push(oldNote);
         }
-        return newArray
-    })
-}
+      }
+      return newArray;
+    });
+  }
 
-function deleteNote(event, noteId) {
-    event.stopPropagation()
-    setNotes(oldNotes => oldNotes.filter(note => note.id !== noteId))
-}
-
+  function deleteNote(event, noteId) {
+    event.stopPropagation();
+    setNotes((oldNotes) => oldNotes.filter((note) => note.id !== noteId));
+  }
 
   function findCurrentNote() {
-      return notes.find(note => {
-          return note.id === currentNoteId
+    return (
+      notes.find((note) => {
+        return note.id === currentNoteId;
       }) || notes[0]
+    );
   }
-  
-  return (
-      <main>
-      {
-          notes.length > 0 
-          ?
-          <Split 
-              sizes={[30, 70]} 
-              direction="horizontal" 
-              className="split"
-          >
-              <Sidebar
-                  notes={notes}
-                  currentNote={findCurrentNote()}
-                  setCurrentNoteId={setCurrentNoteId}
-                  newNote={createNewNote}
-                  deleteNote={deleteNote}
 
+  return (
+    <main>
+      {notes.length > 0 ? (
+        <Split sizes={[30, 70]} direction="horizontal" className="split">
+          <Sidebar
+            notes={notes}
+            currentNote={findCurrentNote()}
+            setCurrentNoteId={setCurrentNoteId}
+            newNote={createNewNote}
+            deleteNote={deleteNote}
+          />
+          {currentNoteId && notes.length > 0 && (
+            <Editor currentNote={findCurrentNote()} updateNote={updateNote} />
+          )}
+        </Split>
+      ) : (
+        <div className="no-notes">
+          <button className="first-note" onClick={createNewNote}>
+            <p>
+              creaтe a noтe{" "}
+              <FontAwesomeIcon
+                icon={icon({
+                  name: "marker",
+                  family: "classic",
+                  style: "solid",
+                })}
               />
-              {
-                  currentNoteId && 
-                  notes.length > 0 &&
-              <Editor 
-                  currentNote={findCurrentNote()} 
-                  updateNote={updateNote} 
-              />
-              }
-          </Split>
-          :
-          <div className="no-notes">
-              <h1>You have no notes</h1>
-              <button 
-                  className="first-note" 
-                  onClick={createNewNote}
-              >
-                  <p>Create one <FontAwesomeIcon icon={icon({name: 'marker', family: 'classic', style: 'solid'})} /></p>
-                  
-              </button>
-          </div>
-          
-      }
-      </main>
-  )
+            </p>
+          </button>
+        </div>
+      )}
+    </main>
+  );
 }
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,35 +1,69 @@
-import React from "react"
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { icon } from '@fortawesome/fontawesome-svg-core/import.macro'
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { icon } from "@fortawesome/fontawesome-svg-core/import.macro";
 
 export default function Sidebar(props) {
-    const noteElements = props.notes.map((note, index) => (
-        <div key={note.id}>
-            <div
-                
-                className={`title ${
-                    note.id === props.currentNote.id ? "selected-note" : ""
-                }`}
-                onClick={() => props.setCurrentNoteId(note.id)}
-            >
-                <h4 className="text-snippet">{note.body.split("\n")[0]}</h4>
-                <button 
-                    className="delete-btn"
-                    onClick={event => props.deleteNote(event, note.id)} //pass parameters (event to event, note.id to noteId) in deleteNote function in App.js
-                >
-                    <i className="gg-trash trash-icon"></i>
-                </button>
-            </div>
-        </div>
-    ))
+  const noteElements = props.notes.map((note, index) => (
+    <div key={note.id}>
+      <div
+        className={`title ${
+          note.id === props.currentNote.id ? "selected-note" : ""
+        }`}
+        onClick={() => props.setCurrentNoteId(note.id)}
+      >
+        <h4 className="text-snippet">{note.body.split("\n")[0]}</h4>
 
-    return (
-        <section className="pane sidebar">
-            <div className="sidebar--header">
-                <h3>Notes <FontAwesomeIcon icon={icon({name: 'clipboard'})} /></h3>
-                <button className="new-note" onClick={props.newNote}>+</button>
-            </div>
-            {noteElements}
-        </section>
-    )
+        <button
+          className="delete-btn"
+          onClick={(event) => props.deleteNote(event, note.id)} //pass parameters (event to event, note.id to noteId) in deleteNote function in App.js
+        >
+          <FontAwesomeIcon
+            icon={icon({ name: "trash" })}
+            className={`title ${
+              note.id === props.currentNote.id ? "selected-note-icon" : ""
+            }`}
+          />
+        </button>
+        <button
+          className="delete-btn"
+          onClick={(event) => props.deleteNote(event, note.id)} //pass parameters (event to event, note.id to noteId) in deleteNote function in App.js
+        >
+          <FontAwesomeIcon
+            icon={icon({ name: "lock" })}
+            className={`title ${
+              note.id === props.currentNote.id ? "selected-note-icon" : ""
+            }`}
+          />
+
+          {/* <FontAwesomeIcon
+            icon={icon({ name: "lock-open" })}
+            style={{
+              color: "white",
+              fontSize: "13px",
+              marginLeft: "8px",
+              cursor: "pointer",
+            }}
+          /> */}
+        </button>
+      </div>
+    </div>
+  ));
+
+  return (
+    <section className="pane sidebar">
+      <div className="sidebar--header">
+        <h3>
+          ℕ𝕆𝕋𝔼𝕊{" "}
+          {/* <FontAwesomeIcon
+            icon={icon({ name: "clipboard" })}
+            style={{ color: "gray", fontSize: "21px" }}
+          /> */}
+        </h3>
+        <button className="new-note" onClick={props.newNote}>
+          +
+        </button>
+      </div>
+      {noteElements}
+    </section>
+  );
 }
