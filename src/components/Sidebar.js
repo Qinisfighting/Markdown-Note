d96dd4bb9944a38a6fadfdaaf704b57d6714ae4f
Yanqin Qualmann <yq.qualmann@gmail.com>
2025-05-25 16:42:48 +0200

    mib

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -57,7 +57,7 @@ export default function Sidebar(props) {
   return (
     <section className="pane sidebar">
       <div className="sidebar--header">
-        <h3>Notes</h3>
+        <h3>N✿tes</h3>
         <button className="new-note" onClick={props.newNote}>
           +
         </button>
